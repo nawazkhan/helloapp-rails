9bda02e5eae8cb5b1cc7ad1c92c7bcf93bb523e9
nawazkhan <nawazahamedkhan@gmail.com>
2017-09-03 10:35:40 -0700

    added bootstrap, images, about page, custom scss, routes, sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 	get '/home', to: 'static_pages#home'
   	get '/help', to: 'static_pages#help'
+  	get '/about', to: 'static_pages#about'
+  	get '/new', to: 'users#new'
   	resources :microposts
   	resources :users
-  	root 'users#index'
+  	root 'static_pages#home'
 end
