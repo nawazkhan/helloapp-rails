bc7cadda82deecfe249eec12b7e7b0179f2ec82a
nawazkhan <nawazahamedkhan@gmail.com>
2017-09-03 09:23:14 -0700

    added named routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	
-	def full_page_title(page_title = '')
+	def full_page_title(page_title)
 		base_title = 'Ruby on Rails sample app'
 	  	if page_title.empty?
 	  		base_title
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  resources :microposts
-  resources :users
-  root 'users#index'
+	get '/home', to: 'static_pages#home'
+  	get '/help', to: 'static_pages#help'
+  	resources :microposts
+  	resources :users
+  	root 'users#index'
 end
