f71a02e62906892de4cdff797771f6bf0fc0c453
nawazkhan <nawazahamedkhan@gmail.com>
2017-09-02 17:47:11 -0700

    added application helper for displaying full page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	
+	def full_page_title(page_title = '')
+		base_title = 'Ruby on Rails sample app'
+	  	if page_title.empty?
+	  		base_title
+	  	else
+	  		page_title + ' | ' + base_title
+	  	end
+	end
 end
